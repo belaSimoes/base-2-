afe1e993c203736a4e639fc53ed0f97c6499593e
Luiz F. Picolo <isabelasimoes205@gmail.com>
2023-05-02 16:59:11 -0400

    adicionado "de" em dataCadastro

--- script.js
+++ script.js
@@ -1,13 +1,13 @@
 // divListaProduto.insertAdjacentHTML('afterbegin', '');
 class Produto {
-    constructor (nome, dataCadastro, descricao, preco){
+    constructor (nome, datadeCadastro, descricao, preco){
     this.nome = nome;
-    this.dataCadastro = dataCadastro;
+    this.datadeCadastro = datadeCadastro;
     this.descricao = descricao;
     this.preco = preco;
 }
 mostrarProdutos (){
-    return this.nome + this.dataCadastro + this.descricao + this.preco ;
+    return this.nome + this.datadeCadastro + this.descricao + this.preco ;
  }
 }
 
@@ -15,12 +15,12 @@ const produto = new Produto("isabela", "27-02-2023", "boneca da Hello Kitty", "R
 console.log(produto.mostrarProdutos())
 
 class ProdutosDestaque extends Produto{
-    constructor (nome, dataCadastro, descricao,  preco, imagemDestaque){
-      super(nome, dataCadastro, descricao, preco, imagemDestaque)
+    constructor (nome, datadeCadastro, descricao,  preco, imagemDestaque){
+      super(nome, datadeCadastro, descricao, preco, imagemDestaque)
       this.imagemDestaque = imagemDestaque;
     }
     mostrarProdutosDestaque (){
-        return this.nome + this.dataCadastro + this.descricao + this.preco + this.imagemDestaque;
+        return this.nome + this.datadeCadastro + this.descricao + this.preco + this.imagemDestaque;
      }
 }   
 const produtosDestaque = new ProdutosDestaque("isabela", "27-02-2023", "boneca da Hello Kitty", "R$ 49,99", "https://www.voceselembra.com/wp-content/uploads/2021/03/Hello-Kitty-1.jpg");
