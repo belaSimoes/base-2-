3f71e87f5f2ac320d55b24c9173b923233f5a1cb
belaSimoes <isabelasimoes205@gmail.com>
2023-05-03 17:26:07 -0400

    inserindo

--- script.js
+++ script.js
@@ -11,7 +11,7 @@ mostrarProdutos (){
  }
 }
 
-const produto = new Produto("isabela", "27-02-2023", "boneca da Hello Kitty", "R$ 49,99");
+const produto = new Produto("Boneca", "27-02-2023", "boneca da Hello Kitty", "R$ 49,99");
 console.log(produto.mostrarProdutos())
 
 class ProdutosDestaque extends Produto{
@@ -20,8 +20,22 @@ class ProdutosDestaque extends Produto{
       this.imagemDestaque = imagemDestaque;
     }
     mostrarProdutosDestaque (){
-        return this.nome + this.datadeCadastro + this.descricao + this.preco + this.imagemDestaque;
+        return `
+        <h1 class = "bela" >${this.nome}</h1>
+        <h4> ${this.datadeCadastro}</h4>
+        <img scr="https://www.voceselembra.com/wp-content/uploads/2021/03/Hello-Kitty-1.jpg">${this.imagemDestaque} <img>
+        <p> ${this.descricao}</p>
+        <p> ${this.preco}</p>
+        ` 
+       // return this.nome + this.datadeCadastro + this.descricao + this.preco + this.imagemDestaque;
      }
 }   
-const produtosDestaque = new ProdutosDestaque("isabela", "27-02-2023", "boneca da Hello Kitty", "R$ 49,99", "https://www.voceselembra.com/wp-content/uploads/2021/03/Hello-Kitty-1.jpg");
+const produtosDestaque = new ProdutosDestaque("Boneca", "27-02-2023", "boneca da Hello Kitty", "R$ 49,99", "https://www.voceselembra.com/wp-content/uploads/2021/03/Hello-Kitty-1.jpg");
 console.log(produtosDestaque.mostrarProdutosDestaque())
+
+
+const produtos = document.getElementById("produto-destaque")
+produtos.insertAdjacentHTML('afterbegin', produtosDestaque.mostrarProdutosDestaque());
+
+//const listaProdutos = document.getElementById("lista-produtos")
+//divListaProduto.insertAdjacentHTML('afterbegin', produtosDestaque.mostrarProdutosDestaque());
