ef3e1b926c0feaaf8df30f50a6da80b80e0d3aaa
belaSimoes <isabelasimoes205@gmail.com>
2023-05-17 17:33:51 -0400

    adicionei imagem

--- script.js
+++ script.js
@@ -1,37 +1,37 @@
-// divListaProduto.insertAdjacentHTML('afterbegin', '');
-class Produto {
-    constructor (nome, datadeCadastro, descricao, preco){
-    this.nome = nome;
-    this.datadeCadastro = datadeCadastro;
-    this.descricao = descricao;
-    this.preco = preco;
+ // divListaProduto.insertAdjacentHTML('afterbegin', '');
+ class Produto {
+  constructor (nome, datadeCadastro, descricao, preco){
+  this.nome = nome;
+  this.datadeCadastro = datadeCadastro;
+  this.descricao = descricao;
+  this.preco = preco;
 }
 mostrarProdutos (){
-    return this.nome + this.datadeCadastro + this.descricao + this.preco ;
- }
+  return this.nome + this.datadeCadastro + this.descricao + this.preco ;
+}
 }
 
 const produto = new Produto("Boneca", "27-02-2023", "boneca da Hello Kitty", "R$ 49,99");
 console.log(produto.mostrarProdutos())
 
 class ProdutosDestaque extends Produto{
-    constructor (nome, datadeCadastro, descricao,  preco, imagemDestaque){
-      super(nome, datadeCadastro, descricao, preco, imagemDestaque)
-      this.imagemDestaque = imagemDestaque;
-    }
-    mostrarProdutosDestaque (){
-        return `
-        <h1 class = "bela" >${this.nome}</h1>
-        <h4> ${this.datadeCadastro}</h4>
-        <img scr="https://www.voceselembra.com/wp-content/uploads/2021/03/Hello-Kitty-1.jpg">${this.imagemDestaque} <img>
-        <p> ${this.descricao}</p>
-        <p> ${this.preco}</p>
-        ` 
-       // return this.nome + this.datadeCadastro + this.descricao + this.preco + this.imagemDestaque;
-     }
+  constructor (nome, datadeCadastro, descricao,  preco, imagemDestaque){
+    super(nome, datadeCadastro, descricao, preco, imagemDestaque)
+    this.imagemDestaque = imagemDestaque;
+  }
+  mostrarProdutosDestaque (){
+      return `
+      <h1 class = "bela" >${this.nome}</h1>
+      <h4> ${this.datadeCadastro}</h4>
+      <img src="${this.imagemDestaque}"/>
+      <p> ${this.descricao}</p>
+      <p> ${this.preco}</p>
+      ` 
+     // return this.nome + this.datadeCadastro + this.descricao + this.preco + this.imagemDestaque;
+   }
 }   
-const produtosDestaque = new ProdutosDestaque("Boneca", "27-02-2023", "boneca da Hello Kitty", "R$ 49,99", "https://www.voceselembra.com/wp-content/uploads/2021/03/Hello-Kitty-1.jpg");
-console.log(produtosDestaque.mostrarProdutosDestaque())
+const produtosDestaque = new ProdutosDestaque("Boneca", "27-02-2023", "boneca da Hello Kitty", "R$ 49,99", "https://t1.uc.ltmcdn.com/pt/posts/5/6/5/desenhos_da_hello_kitty_para_colorir_9565_600.jpg");
+//console.log(produtosDestaque.mostrarProdutosDestaque())
 
 
 const produtos = document.getElementById("produto-destaque")
